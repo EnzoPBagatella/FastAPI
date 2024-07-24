ef92659e100521fbd8ac7276a4237b221d41c68b
Enzo Pomaro Bagatella <ep.bagatella@hotmail.com>
2024-07-24 16:03:44 -0300

    Adicionado o router.put para troca de senha do usuario

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -25,13 +25,30 @@ db_dependecy = Annotated[Session, Depends(get_db)]
 user_dependecy = Annotated[dict, Depends(get_current_user)]
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated = 'auto')
 
+
+class UserVerification(BaseModel):
+    password: str
+    new_password: str = Field(min_length=6)
+
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_user(user:user_dependecy, db:db_dependecy):
     query = db.query(Users).filter(Users.id == user.get('id')).first()
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
     return query
-    
+
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependecy, db: db_dependecy, user_verification: UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='Error on password change')
+    user_model.hashed_password= bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
+
 #@router.get('/',status_code=status.HTTP_200_OK)
 #async def get_user(user:user_dependecy, db:db_dependecy):
     #if user is None:
